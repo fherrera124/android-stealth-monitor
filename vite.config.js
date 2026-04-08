6f958566d2054fea6b79e95dfa52c0cc13201ae8
Francisco Herrera <fran.herrera124@gmail.com>
2026-04-08 14:00:39 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: './frontend/index.html'
     },
     outDir: './nginx/public',
-    emptyOutDir: true
+    emptyOutDir: true,
+    publicDir: false
   }
 })
