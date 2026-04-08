49bb34c525a8d6aa7d6f2da326e4b40426d7df63
Francisco Herrera <fran.herrera124@gmail.com>
2026-04-08 11:40:16 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: './server/frontend',
   build: {
-    rollupOptions: {
-      input: './server/frontend/index.html'
-    },
     outDir: './public',
     emptyOutDir: true
   }
